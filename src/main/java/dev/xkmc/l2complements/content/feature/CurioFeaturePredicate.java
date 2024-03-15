5b9729128bd6a689a2aa30ad1fdc926a6e992606
lcy0x1 <chengyilin@umail.ucsb.edu>
2024-03-15 23:01:48 +0700

    tag

--- src/main/java/dev/xkmc/l2complements/content/feature/CurioFeaturePredicate.java
+++ src/main/java/dev/xkmc/l2complements/content/feature/CurioFeaturePredicate.java
@@ -2,6 +2,7 @@ package dev.xkmc.l2complements.content.feature;
 
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.ForgeRegistries;
 import top.theillusivec4.curios.api.CuriosApi;
 
 import java.util.function.Supplier;
@@ -10,6 +11,7 @@ public record CurioFeaturePredicate(Supplier<Item> item) implements FeaturePredi
 
 	@Override
 	public boolean test(LivingEntity e) {
+		ForgeRegistries.ITEMS.tags().getReverseTag(item.get()).get().getTagKeys();
 		return CuriosApi.getCuriosInventory(e).resolve().flatMap(x -> x.findFirstCurio(item.get())).isPresent();
 	}
 
